47c2d4c16cdecd80c485aaacebe5aca890d78266
Ernest Wu <wuboys@gmail.com>
2015-03-20 23:46:00 +0800

    test locale

---
+++ app/assets/javascripts/refinery/i18n/localautosave/zh_TW.js
@@ -0,0 +1,8 @@
+tinyMCE.addI18n('en', {
+	'localautosave.restoreContent' : '載入內容',
+	'localautosave.chooseVersion' : '選擇你要載入的版本',
+	'localautosave.chars' : '字元',
+	'localautosave.clearAll' : '捨棄全部',
+	'localautosave.noContent' : '目前沒有任何內容',
+	'localautosave.ifRestore' : '如果你載入這個版本，目前的內容將會覆蓋，確定嗎？'
+});
---
+++ app/assets/javascripts/refinery/i18n/youtube/zh_TW.js
@@ -0,0 +1,13 @@
+tinymce.addI18n('en',{
+    'YouTube Tooltip'   : "YouTube",
+	'YouTube Title'     : "置入youtube影片",
+	'Youtube URL'	    : '要分享的youtube連結',
+	'Youtube ID'        : '您可使用以下格式 : http://youtu.be/xxxxxxxx or http://www.youtube.com/watch?v=xxxxxxxx',
+	'width'			    : '寬',
+	'height'		    : '高',
+	'autoplay'		    : '自動播放',
+	'Related video'     : '相關影片',
+	'HD video'          : '以HD畫質觀看',
+    'html5'             : 'HTML5',
+    'Insert'            : '置入'
+});
